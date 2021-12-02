7a2f35be81ddadc327f6063aebc1fc1feacc44c8
Dude <michael_runschke@gmx.de>
2021-12-02 16:48:37 +0100

    konto1/ konto2/ konto3 + Kontostand 10000

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,11 +4,43 @@ public class App {
     
     public static void main(String[] args) {
 
-        Konto konto = new Konto(); 
-        System.out.println("Blick von aussen: " + konto);
+        Konto konto1 = new Konto(); // Klasse --> Objekt / Instanz
+        System.out.println("Blick von aussen: " + konto1);
 
-        konto.tellYourAddress();
+        konto1.name = "1.Konto";
+        konto1.tellYourAddress();
+        konto1.kontostand = 10000;
+
+        System.out.println(konto1.tellYourName());
+        System.out.println(konto1.kontostand);
+
+        System.out.println("-------------------------------------------------------");
+
+
+        Konto konto2 = new Konto(); // Klasse --> Objekt / Instanz
+        System.out.println("Blick von aussen: " + konto2);
+
+        konto2.name = "2.Konto";
+        konto2.tellYourAddress();
+        konto2.kontostand = 10000;
+
+        System.out.println(konto2.tellYourName());
+        System.out.println(konto2.kontostand);
    
+
+        System.out.println("-------------------------------------------------------");
+
+
+        Konto konto3 = new Konto(); // Klasse --> Objekt / Instanz
+        System.out.println("Blick von aussen: " + konto3);
+
+        konto3.name = "3.Konto";
+        konto3.tellYourAddress();
+        konto3.kontostand = 10000;
+
+        System.out.println(konto3.tellYourName());
+        System.out.println(konto3.kontostand);
+
     }
 
 }
--- src/com/cc/java/Konto.java
+++ src/com/cc/java/Konto.java
@@ -2,7 +2,15 @@ package com.cc.java;
 
 public class Konto {
     
+    public String name;
+    public int kontostand = 10000;
+
     public void tellYourAddress(){
         System.out.println("Blick von innen: " + this);
+     }
+
+    public String tellYourName(){
+       return this.name;
     }
+
 }

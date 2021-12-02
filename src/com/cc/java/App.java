8e9aa117e8535588353c089d12a4c87741f0d93c
Dude <michael_runschke@gmx.de>
2021-12-02 16:23:43 +0100

    The Beginning

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,7 +4,11 @@ public class App {
     
     public static void main(String[] args) {
 
-    
+        Konto konto = new Konto(); 
+        System.out.println("Blick von aussen: " + konto);
+
+        konto.tellYourAddress();
+   
     }
 
 }
---
+++ src/com/cc/java/Konto.java
@@ -0,0 +1,8 @@
+package com.cc.java;
+
+public class Konto {
+    
+    public void tellYourAddress(){
+        System.out.println("Blick von innen: " + this);
+    }
+}
